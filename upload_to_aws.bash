9957abdc0c76cc4a0216599482f4019cdbeee76d
quang-tran-belle-ai <81731874+quang-tran-belle-ai@users.noreply.github.com>
2021-05-31 21:20:35 -0400

    fix bugs

--- upload_to_aws.bash
+++ upload_to_aws.bash
@@ -2,5 +2,6 @@
 
 scp package-lock.json idris-frontend:~/idris/
 scp package.json idris-frontend:~/idris/
+scp update.bash idris-frontend:~/idris/
 scp -r public idris-frontend:~/idris/
 scp -r src idris-frontend:~/idris/
